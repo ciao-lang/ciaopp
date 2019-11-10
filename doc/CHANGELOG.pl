8dfbdb52feacfaf7640d891f91c8a9246a823c47
Jose F. Morales <jfmcjf@gmail.com>
2019-11-10 09:38:11 +0100

    (ciaopp) Minor pass over candidate CHANGELOG.pl, updated CHANGELOG
    Src-commit: 8c99971a99f1b2a9a8afc2be2f4cdabd8f6cc252

--- doc/CHANGELOG.pl
+++ doc/CHANGELOG.pl
@@ -1,54 +1,67 @@
-:- doc(version(1*3+0,2019/11/3,11:16*23+'CEST'),
-	"% TODO: publish ciaopp_batch?
-@section{Functionality}
-        @begin{itemize}
-      @item Ported incremental analysis of ciaopp 0.8.
-      @item Added a high level interface for incanal.
-      @item Integrated incremental and modular analysis (pp_flag incremental=on)
-      @item Split tests in a bundle (ciaopp_tests).
-      @item ciaopp-dump-cmp: new command to compare two ciaopp analaysis dumps.
-      @item ciaopp-dump-fmt: new command that outputs the analysis in dot format.
-      @item New flag value: Raw analysis printer (pp_flag output_lang=raw).
-      @item Added analysis using run-time semantics of assertions to the dd fixpoint (pp_flag old_trusts=off).
-      @item Fixes in meta_calls.
-      @item Added hook to cache library assertions.
-      @item Added pp flag to trace fixpoints.
-      @item Added generic, non relational domain with simpler domain operations.
-      @item Modularized domains now they use the aidomain package.
-      @item Integrated modular analysis in the general analysis driver.
-      @item New flag value: restrict modular analysis to the modules of a bundle (pp_flag punit_boundary=bundle).
-      @item Added flag to load modules faster by preloading ciao libraries.
-        @end{itemize}
+% TODO: publish ciaopp_batch?
 
-@section{Performance}
-        @begin{itemize}
-     @item Fixed dangling choicepoints across all modules.
-     @item Removed api (now using p_unit).
+:- doc(version(1*3+0,2019/11/3,11:16*23+'CEST'), "
+   @begin{itemize}
+   @item New functionality:
+     @begin{itemize}
+     @item Backported (and improved) incremental analysis (incanal)
+       from CiaoPP 0.8.
+     @item Integrated incremental and modular analysis (@tt{pp_flag
+       incremental=on})
+     @item Integrated modular analysis in the general analysis driver.
+     @item Added analysis using run-time semantics of assertions to
+       the @tt{dd} fixpoint (@tt{pp_flag} @tt{old_trusts=off}).
+     @item Fixes in meta-calls (work in progress).
+     @item Modularized domains, now they use the aidomain package
+       (work in progress).
+     @item Added generic, non relational domain with simpler domain operations.
+     @item New flag value: restrict modular analysis to the modules of
+       a bundle (@tt{pp_flag} @tt{punit_boundary=bundle}).
+     @end{itemize}
+   @item Performance improvements:
+     @begin{itemize}
+     @item Added hook to cache library assertions.
+     @item Added flag to load modules faster by preloading ciao libraries.
+     @item Fixed many dangling choicepoints across all modules.
      @item Fixed performance issues in modular analysis.
-     @item Fixed bug of metacalls inside a recursion (fixpo dd).
-        @end{itemize}
-
-@section{Other}
-        @begin{itemize}
-     @item Created web interface for demos.
+     @item Fixed bug of metacalls inside a recursion (@tt{dd} fixpoint).
+     @end{itemize}
+   @item User interface and documentation:
+     @begin{itemize}
      @item Rearranged manual.
-     @item Created tests for gitlab.
-     @item Fixed maintaining plai_db for complete operations
+     @item Created web interface for demos (see @tt{ciaopp_online} bundle).
+     @item Added a high level interface for intermental analisis (incanal).
+     @end{itemize}
+   @item Benchmarks, internal testing, and debugging:
+     @begin{itemize}
+     @item Tests/benchmarks moved to a separate bundle (ciaopp_tests).
+     @item Added tests for gitlab CI (continuous integration).
      @item Some fixes in the davinci interface.
-     @item Using p_unit/program_keys.pl for the keys of ciaopp.
-     @item Fixed (documentation) assertions. CiaoPP can be run with the package rtchecks.
+     @item Fixed (documentation) assertions. CiaoPP can be run with
+       the package rtchecks.
      @item Added analysis_stats.pl to unify statistics collecting.
-        @end{itemize}
-
-@section{Minor}
-        @begin{itemize}
-     @item Fixed classifying mistakenly recursive predicates as non recursive (meta predicates).
+     @item ciaopp-dump-cmp: new command to compare two ciaopp analaysis dumps.
+     @item ciaopp-dump-fmt: new command that outputs the analysis in dot format.
+     @item New flag value: Raw analysis printer (@tt{pp_flag}
+       @tt{output_lang=raw}).
+     @item Added @tt{pp_flag} to trace fixpoints.
+     @end{itemize}
+   @item Other bug fixes, cleanups, refactoring, etc.:
+     @begin{itemize}
+     @item Removed @tt{api} (now using @tt{p_unit}).
+     @item Fixed maintaining @tt{plai_db} for complete operations
+     @item Fixed classifying mistakenly recursive predicates as non
+       recursive (meta predicates).
      @item Fixed issue with reexported predicates in p_unit.
-     @item Fixed problem with meta_predicates that did not have any meta argument.
-     @item Added 'free/1' to sharefree_clique domain.
-     @item Replaced runtime_control:statistics/2 by analysis_stats:pp_statistics/2.
-        @end{itemize}
-      ").
+     @item Fixed problem with meta_predicates that did not have any
+       meta argument.
+     @item Added @tt{free/1} to @tt{sharefree_clique} domain.
+     @item Replaced @tt{runtime_control:statistics/2} by
+       @tt{analysis_stats:pp_statistics/2}.
+     @item Using @tt{p_unit/program_keys.pl} for the keys of ciaopp.
+     @end{itemize}
+   @end{itemize}
+").
 
 :- doc(version(1*2+0,2006/01/03,11:16*23+'CEST'),
 	"New version").
