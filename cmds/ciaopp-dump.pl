9e2a3a32e215a4bb157f2aacd5b00d237b76537c
Isabel García <isabel.garcia@imdea.org>
2020-03-16 23:00:13 +0100

    (ciaopp) (ciaopp_extra) Improve doc of ciaopp-dump, fix manual
    Src-commit: 34e84aa1229b5489967650cfbe976abac88e1d09

--- cmds/ciaopp-dump.pl
+++ cmds/ciaopp-dump.pl
@@ -1,18 +1,60 @@
-:- module(_, [main/1], []).
+:- module(_, [main/1], [assertions]).
+
+:- doc(title,"Display information about CiaoPP files").
+
+:- doc(author,"Isabel Garcia-Contreras").
+
+:- use_module(library(format)).
 
 :- use_module('ciaopp-dump-cmp').
 :- use_module('ciaopp-dump-fmt').
 :- use_module('ciaopp-dump-stats').
 :- use_module('ciaopp-dump-syntactic').
 
-main(['fmt'|Args]) :-
+:- doc(module, "This program outputs information about CiaoPP files.
+
+   @section{Usage}
+
+   @begin{verbatim}
+@includefact{usage_text/1}
+   @end{verbatim}
+").
+
+usage_text("ciaopp-dump <action> [<opts>] <files>
+
+where the possible options are:
+
+  -h
+     Print this information
+
+  cmp <path1> <path2> <domain>
+      compares the analysis results of path1 and path2, which may be directories or @tt{.dump} files
+
+  fmt <filename>[.dump]
+     Outputs the analysis result in dot format
+
+  stats [--print-header] <filenames>[.dump]
+     Counts simple characteristics of analysis in the files.
+
+     --print-header: prints the names of the characteristics displayed
+
+  syntactic [--print-header] <filenames>[.pl]
+     Counts simple syntactic characteristics of the source in the files.
+
+     --print-header: prints the names of the characteristics displayed
+").
+
+
+main(['-h']) :- !,
+    usage_text(T),
+    format(user_output,"Usage: ~s~n",[T]).
+main(['fmt'|Args]) :- !,
     'ciaopp-dump-fmt':main(Args).
-
-main(['cmp'|Args]) :-
+main(['cmp'|Args]) :- !,
     'ciaopp-dump-cmp':main(Args).
-
-main(['stats'|Args]) :-
+main(['stats'|Args]) :- !,
     'ciaopp-dump-stats':main(Args).
-
-main(['syntactic'|Args]) :-
+main(['syntactic'|Args]) :- !,
     'ciaopp-dump-syntactic':main(Args).
+main(_) :-
+    format(user_error, "Wrong aruments. Run '-h' to show help",[]).
