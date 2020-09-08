88b3b7ec7c219d9b15f494604609c23235f5da0c
Isabel Garcia <isabel.garcia@imdea.org>
2020-09-08 17:25:39 +0200

    (ciaopp) format unexpand.pl
    Src-commit: e04dccef7d3c067e2234cd8c98142b60c73f3af5

--- src/p_unit/unexpand.pl
+++ src/p_unit/unexpand.pl
@@ -41,83 +41,79 @@
 
 % -------------------------------------------------------------------------
 
-transform_clause_list( [] , _M , [] ).
-transform_clause_list( [clause( H , B ) | C ] ,M, [clause( HT , BT ) | CR] ) :-
-    transform_clause( H, B, M, HT, BT),
-    transform_clause_list( C , M , CR ).
+transform_clause_list([], _M, []).
+transform_clause_list([clause(H, B)|C],M, [clause(HT, BT)|CR]) :-
+    transform_clause(H, B, M, HT, BT),
+    transform_clause_list(C, M, CR).
 
-transform_clause( H, B, M, H1, B1):-
-    transform_head( H , M, H1 ),
-    unexpand_meta_calls( B , BT ),
-    transform_body( BT , M, B1 ),
+transform_clause(H, B, M, H1, B1):-
+    transform_head(H, M, H1),
+    unexpand_meta_calls(B, BT),
+    transform_body(BT, M, B1),
     !.
-transform_clause( H, B, _, H, B):-
+transform_clause(H, B, _, H, B):-
     throw(unexpansion_fails(H,B)).
 
-%transform_body( true , true ).
+%transform_body(true, true).
 % DTM: WITHOUT THIS clause we could have infinity loop!!!
-transform_body( A, _, A ) :-
-    var(A),
-    !.
-transform_body( ','(A,B), M, ','(AT,BT) ) :-
-    !,
-    transform_body( A, M, AT ),
-    transform_body( B, M, BT ).
-transform_body( '&'(A,B), M, '&'(AT,BT) ) :-
-    !,
-    transform_body( A, M, AT ),
-    transform_body( B, M, BT ).
+transform_body(A, _, A) :-
+    var(A), !.
+transform_body(','(A,B), M, ','(AT,BT)) :- !,
+    transform_body(A, M, AT),
+    transform_body(B, M, BT).
+transform_body('&'(A,B), M, '&'(AT,BT)) :- !,
+    transform_body(A, M, AT),
+    transform_body(B, M, BT).
 % ACC: Can be removed if the output of the parallelizers is transformed
-transform_body( '->'(A,B), M, '->'(AT,BT) ) :-
-    !,
-    transform_body( A, M, AT ),
-    transform_body( B, M, BT ).
+transform_body('->'(A,B), M, '->'(AT,BT)) :- !,
+    transform_body(A, M, AT),
+    transform_body(B, M, BT).
 % DTM: This is necesary for assertion body disjunction 
-transform_body( Disj, M, DO ) :-
+transform_body(Disj, M, DO) :-
     Disj = ';'(A,_),
     list(A),
     !,
-    transform_body_disj( Disj , M , DO ).
-transform_body( ';'(A,B), M, ';'(AT,BT) ) :-
+    transform_body_disj(Disj, M, DO).
+transform_body(';'(A,B), M, ';'(AT,BT)) :-
     !,
-    transform_body( A, M, AT ),
-    transform_body( B, M, BT ).
-transform_body( '->'(A,B), M, '->'(AT,BT) ) :-
+    transform_body(A, M, AT),
+    transform_body(B, M, BT).
+transform_body('->'(A,B), M, '->'(AT,BT)) :-
     !,
-    transform_body( A, M, AT ),
-    transform_body( B, M, BT ).
-transform_body( H , M, HT ) :-
-    transform_name( H , M, HT ).
-%transform_body( A , A ).
+    transform_body(A, M, AT),
+    transform_body(B, M, BT).
+transform_body(H, M, HT) :-
+    transform_name(H, M, HT).
+%transform_body(A, A).
 
-transform_body_disj( (A;B) , M , (AT;BT) ) :-
+transform_body_disj((A;B), M, (AT;BT)) :-
     !,
-    transform_body_disj( A , M , AT ),
-    transform_body_disj( B , M , BT ).
-transform_body_disj( A , M , AT ) :-
-    list_to_conj( A , AC ),
-    transform_body( AC, M, ATM ),
-    list_to_conj( AT , ATM ).
+    transform_body_disj(A, M, AT),
+    transform_body_disj(B, M, BT).
+transform_body_disj(A, M, AT) :-
+    list_to_conj(A, AC),
+    transform_body(AC, M, ATM),
+    list_to_conj(AT, ATM).
 
 
-transform_name( H , user(_), HT ) :-
-    transform_name( H , user , HT ),!.
-transform_name( H , M, HT ) :-
-    transform_metapred( H, M, HT0 ),
+transform_name(H, user(_), HT) :-
+    transform_name(H, user, HT),!.
+transform_name(H, M, HT) :-
+    transform_metapred(H, M, HT0),
     HT0 =.. [ F | A ],
-    functor( HT0 , F , N ),
-    transform_atom( F , N , M , FT ),
-    reconstruct( FT , A , HT ).
+    functor(HT0, F, N),
+    transform_atom(F, N, M, FT),
+    reconstruct(FT, A, HT).
 
-reconstruct( M:FT , A , M:HT ):- !,
+reconstruct(M:FT, A, M:HT):- !,
     HT =.. [ FT | A ].
-reconstruct( FT , A , HT ):-
+reconstruct(FT, A, HT):-
     HT =.. [ FT | A ].
 
-transform_terms( [], [], _M, [] ).
-transform_terms( [_|As0], [remove|Ts], M, As ):-
-    transform_terms( As0, Ts, M, As ).
-transform_terms( [A0|As0], [T|Ts], M, [A|As] ):-
+transform_terms([], [], _M, []).
+transform_terms([_|As0], [remove|Ts], M, As):-
+    transform_terms(As0, Ts, M, As).
+transform_terms([A0|As0], [T|Ts], M, [A|As]):-
     T \== ?,
     nonvar(A0), !,
     % JF: very dirty hack -- add extra arguments to unexpand the predicate properly...
@@ -131,22 +127,22 @@ transform_terms( [A0|As0], [T|Ts], M, [A|As] ):-
         remove_extra_args(ExtraArity, A2, A)
     ; A = A2
     ),
-    transform_terms( As0, Ts, M, As ).
-transform_terms( [A|As0], [_|Ms], M, [A|As] ):-
-    transform_terms( As0, Ms, M, As ).
+    transform_terms(As0, Ts, M, As).
+transform_terms([A|As0], [_|Ms], M, [A|As]):-
+    transform_terms(As0, Ms, M, As).
 
 % TODO: remove
-%% :- pred count( L , B , C )
+%% :- pred count(L, B, C)
 %% 
 %% # "@var{C} is the number of times that @var{B} is in @var{L}.".
 %% 
-%% count( [] , _ , 0 ).
-%% count( [A|As] , A , C ) :-
+%% count([], _, 0).
+%% count([A|As], A, C) :-
 %%      !,
 %%      C is C1 + 1,
-%%      count( As , A , C1 ).
-%% count( [_|As] , A , C ) :-
-%%      count( As , A , C ).
+%%      count(As, A, C1).
+%% count([_|As], A, C) :-
+%%      count(As, A, C).
 
 % ---------------------------------------------------------------------------
 
@@ -157,13 +153,13 @@ arguments marked in @tt{meta_predicate} declaration. For example:
 @begin{verbatim}
 Program:
 (...)
-:- meta_predicate p( ?, addmodule ).
+:- meta_predicate p(?, addmodule).
 
-p( X ) :- 
+p(X) :- 
 (...)
 
 Top-Level:
-?- transform_metapred( 'test:p'(X,caller), A ).
+?- transform_metapred('test:p'(X,caller), A).
 
 A = 'test:p'(X) ? ;
 
@@ -177,26 +173,25 @@ transform_metapred(H, M, HT) :-
     functor(H, F, FArity),
     ( (type_of_goal(metapred(_Type,Meta),_),
        functor(Meta, F, MetaArity)),
-       % this is something we have to keep in mind.
-       % When a metapredicate has addmodule option, 
-       % 2 things can happen:
-       % (A) We read the goal from ciao, so ciao 
-       %     expanded the goal, so the goal has more 
-       %     arguments than metapredicate definition.
-       % (B) A CiaoPP transformation added the goal,
-       %     so the goal has the same number of 
-       %     arguments than metapred declaration.
-       ( FArity = MetaArity
-       ->  % case B
-           Meta =.. [ _ | Metaterms ]
-       ;   % case A
-           meta_to_list( Meta , Metaterms )
-       ),
-       transform_terms( A, Metaterms, M, A0 )
-     ->
-       true
-     ; A0 = A
-    ),
+      % this is something we have to keep in mind.
+      % When a metapredicate has addmodule option, 
+      % 2 things can happen:
+      % (A) We read the goal from ciao, so ciao 
+      %     expanded the goal, so the goal has more 
+      %     arguments than metapredicate definition.
+      % (B) A CiaoPP transformation added the goal,
+      %     so the goal has the same number of 
+      %     arguments than metapred declaration.
+      (FArity = MetaArity  ->  % case B
+          Meta =.. [ _ | Metaterms ]
+      ;   % case A
+          meta_to_list(Meta, Metaterms)
+      ),
+      transform_terms(A, Metaterms, M, A0)
+      ->
+          true
+      ; A0 = A
+   ),
     reconstruct(F, A0, HT).
 
 % ---------------------------------------------------------------------------
@@ -217,12 +212,12 @@ remove_extra_args(N, A0, A) :-
 % ---------------------------------------------------------------------------
 
 % TODO: This code drops _M (because it can be 'multifile')
-transform_head( H , _M, HT ) :- % any M (could be multifile:)
-    H =.. [ F | A ],
+transform_head(H, _M, HT) :- % any M (could be multifile:)
+    H =.. [F|A],
     module_split(F, _MIgnore, FT),
     !,
-    HT =.. [ FT | A ].
-transform_head( H , _M, H ).
+    HT =.. [FT|A].
+transform_head(H, _M, H).
 
 % Here we can translate from 'module:clause' to 'clause'
 
@@ -232,24 +227,24 @@ transform_atom(F, A, _ModuleName, MCT) :-
     atom(F),
     module_split(F, Q, CT),
     !,
-    transform_reexported_atom(F , A , Q , M ),
+    transform_reexported_atom(F, A, Q, M),
     simplify_qualify(M:CT, A, MCT). % JF
 transform_atom(F, _A, _ModuleName, F).
 
-transform_reexported_atom(F , A , Q , M ):-
+transform_reexported_atom(F, A, Q, M):-
     functor(Goal,F,A),
     type_of_goal(imported(Import),Goal),
     % Q is the one from which it is effectively imported
     module_spec(Import,Q),
     !,
     M=Q.
-transform_reexported_atom(F , A , _Q , M ):-
+transform_reexported_atom(F, A, _Q, M):-
     functor(Goal,F,A),
     % it is probably reexported, take anyone:
     type_of_goal(imported(Import),Goal),
     !,
     module_spec(Import,M).
-transform_reexported_atom(_F , _A , M , M ).
+transform_reexported_atom(_F, _A, M, M).
 
 % ---------------------------------------------------------------------------
 % JF {
@@ -262,35 +257,34 @@ transform_reexported_atom(_F , _A , M , M ).
 :- use_module(ciaopp(p_unit/itf_db), [current_itf/3]).
 :- use_module(library(pathnames), [path_basename/2]).
 
-generate_unexpanded_data( user(File) ) :-
+generate_unexpanded_data(user(File)) :-
     !, %% DTM: To support user files
-    path_basename( File , Module ),
-    generate_unexpanded_data( Module ).
+    path_basename(File, Module),
+    generate_unexpanded_data(Module).
 generate_unexpanded_data(Module) :-
     % Generate inverse table for defines 
     current_itf(defines, MF, A),
-      module_split(MF, Module, F),
-      assertz_fact(unexpanded_defines(F, A)), % TODO: missing Module? (for multimod)
-      fail.
+    module_split(MF, Module, F),
+    assertz_fact(unexpanded_defines(F, A)), % TODO: missing Module? (for multimod)
+    fail.
 generate_unexpanded_data(_) :-
     % Generate inverse table for imports
     type_of_goal(imported(_ISpec), Goal),
-%         module_spec(ISpec, IM),
-      functor(Goal, MF, A),
-      module_split(MF, IM, F),
-      functor(GoalReexp, F, A),
-      (current_itf(imports, GoalReexp, r(IM2,IM))-> true ; IM2=IM),
-      \+ unexpanded_import(F, A, IM2),
-      assertz_fact(unexpanded_import(F, A, IM2)),
-      fail.
+    %         module_spec(ISpec, IM),
+    functor(Goal, MF, A),
+    module_split(MF, IM, F),
+    functor(GoalReexp, F, A),
+    (current_itf(imports, GoalReexp, r(IM2,IM))-> true ; IM2=IM),
+    \+ unexpanded_import(F, A, IM2),
+    assertz_fact(unexpanded_import(F, A, IM2)),
+    fail.
 generate_unexpanded_data(_).
 
 clean_unexpanded_data :-
     retractall_fact(unexpanded_defines(_, _)),
     retractall_fact(unexpanded_import(_, _, _)).
 
-simplify_qualify('multifile':F, _A, F):- 
-    !.
+simplify_qualify('multifile':F, _A, F):-  !.
 simplify_qualify(M:F, A, F) :-
     functor(Goal, F, A),
     (current_itf(imports, Goal, r(M2,M));M2=M),
@@ -302,29 +296,29 @@ simplify_qualify(M:F, A, M2:F) :-
     !.
 simplify_qualify(MF, _, MF).
 
-% superfluous_qualify( M , F, A) :-
-%       display( superfluous_qualify( M , F, A) ) , nl,
-%       functor( Goal , F , A ),
-%       current_itf(imports , Goal , r(_,EM) ),
+% superfluous_qualify(M, F, A) :-
+%       display(superfluous_qualify(M, F, A)), nl,
+%       functor(Goal, F, A),
+%       current_itf(imports, Goal, r(_,EM)),
 %       module_split(F, _, Pred),
 %       !,
 %       module_concat(EM, Pred, NF),
-%       superfluous_qualify( M , NF, A).
-superfluous_qualify( M , F, A) :-
+%       superfluous_qualify(M, NF, A).
+superfluous_qualify(M, F, A) :-
     ( unexpanded_defines(F, A) ->
-      ( \+ unexpanded_import(F, A, _) ->
-        % defined in module and not imported from any module
-        true 
-      ;  
-        % (defined in module and) imported from a module, BUT the
-        % qualification refers to the current module
-        curr_file( _ , M ),
-        % This is to avoid warning
-        add_directive( redefining(F/A) ) % TODO: Hmmm avoid this!?
-      )
+        (\+ unexpanded_import(F, A, _) ->
+            % defined in module and not imported from any module
+            true 
+        ;  
+            % (defined in module and) imported from a module, BUT the
+            % qualification refers to the current module
+            curr_file(_, M),
+            % This is to avoid warning
+            add_directive(redefining(F/A)) % TODO: Hmmm avoid this!?
+        )
     ; % (not defined in module and) imported only from one module
-      unexpanded_import(F, A, M),
-      \+ ( unexpanded_import(F, A, M2), M2 \== M )
+        unexpanded_import(F, A, M),
+        \+ (unexpanded_import(F, A, M2), M2 \== M)
     ).
 % JF }
 
@@ -332,23 +326,21 @@ superfluous_qualify( M , F, A) :-
 
 :- pred add_unexpanded_data(Module, Functor, Arity, CurrModule) ::
     (atm(Module), atm(Fuctor), num(Arity))
- # "Add necessary internal data to do a correct unexpansion. User _do
+   # "Add necessary internal data to do a correct unexpansion. User _do
    not have_ to use this predicate.".
 % The same predicated as one imported
-add_unexpanded_data( Module , Functor , Arity , _CurrModule ) :-
+add_unexpanded_data(Module, Functor, Arity, _CurrModule) :-
     unexpanded_import(Functor, Arity, Module),
     !.
 % The predicate is already defined in this module
-add_unexpanded_data( Module , Functor , Arity , Module ) :-
+add_unexpanded_data(Module, Functor, Arity, Module) :-
     unexpanded_defines(Functor, Arity),
     !.
 % New Predicate defines in _this_ module
-add_unexpanded_data( M , Functor , Arity , M ) :-
-    !,
+add_unexpanded_data(M, Functor, Arity, M) :- !,
     assertz_fact(unexpanded_defines(Functor, Arity)).
 % New Predicate imported from other module
-add_unexpanded_data( Module , Functor , Arity , _CurrModule ) :-
-    !,
+add_unexpanded_data(Module, Functor, Arity, _CurrModule) :-  !,
     assertz_fact(unexpanded_import(Functor, Arity, Module)).
 
 % DTM }
@@ -401,7 +393,7 @@ last_datum([X|L],Old,Tail,New):- !,
 
 % -------------------------------------------------------------------------
 
-transform_assrt_body( Body , M , BodyT ) :-
+transform_assrt_body(Body, M, BodyT) :-
     assertion_body(Pred,Compat,Call,Succ,Comp,Comm,Body),
     transform_head(Pred,M,PredT),
     transform_assrt_field(Compat,M,CompatT),
