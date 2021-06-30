c267e1cb770b4b3ec0e7f9ddde0b3ed07605d24c
Jose F. Morales <jfmcjf@gmail.com>
2021-06-30 21:18:32 +0200

    (ciaopp) Use builder_aux:generate_config_file/2
    Src-commit: d2e48ee67028f121e9b1e7bd05de3c99461ce438

--- Manifest/ciaopp_config.hooks.pl
+++ Manifest/ciaopp_config.hooks.pl
@@ -1,40 +1,22 @@
 % (included file)
 
-% TODO: duplicated, move to builder (using bundle deps marked as 'optional')
+% TODO: extract HasBundles from bundle deps marked as optional
 % TODO: due to a (yet to be fixed) cyclic dependency, this code is
 %   included several times
 
-:- use_module(library(bundle/bundle_flags), [get_bundle_flag/2]).
-:- use_module(engine(internals), ['$bundle_id'/1]).
-:- use_module(library(aggregates), [findall/3]).
 :- use_module(library(bundle/bundle_paths), [bundle_path/3]).
-:- use_module(ciaobld(builder_aux), [update_file_from_clauses/3]).
+:- use_module(ciaobld(builder_aux), [generate_config_auto/2]).
 
 create_ciaopp_config :-
     ConfigFile = ~bundle_path(ciaopp, 'src/ciaopp_config_auto.pl'),
-    update_file_from_clauses(~findall(C, emit_config(C)), ConfigFile, _).
-
-emit_config(C) :-
-    '$bundle_id'(ciaopp_cost),
-    C = (:- compilation_fact(has_ciaopp_cost)).
-emit_config(C) :-
-    '$bundle_id'(ciaopp_extra),
-    C = (:- compilation_fact(has_ciaopp_extra)).
-emit_config(C) :-
-    '$bundle_id'(ciaopp_llvm),
-    C = (:- compilation_fact(has_ciaopp_llvm)).
-emit_config(C) :-
-    '$bundle_id'(ciaopp_java),
-    C = (:- compilation_fact(has_ciaopp_java)).
-emit_config(C) :-
-    '$bundle_id'(ciao_ppl),
-    yes = ~get_bundle_flag(ciao_ppl:enabled), % (it can be disabled)
-    C = (:- compilation_fact(has_ciao_ppl)).
-emit_config(C) :-
-    '$bundle_id'(mathematica),
-    yes = ~get_bundle_flag(mathematica:enabled), % (it can be disabled)
-    C = (:- compilation_fact(has_mathematica)).
-emit_config(C) :-
-    '$bundle_id'(davinci),
-    C = (:- compilation_fact(has_davinci)).
+    HasBundles = [
+        ciaopp_cost,
+        ciaopp_extra,
+        ciaopp_llvm,
+        ciaopp_java,
+        ciao_ppl,
+        mathematica,
+        davinci
+    ],
+    generate_config_auto(ConfigFile, HasBundles).
 
