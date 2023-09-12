9bf1a5fe590d0eee20ec03a780593cd70e48e8ce
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2023-09-12 11:21:18 +0200

    (ciaopp) Eliminated verifly example that was duplicated. Minor correction.
    Src-commit: b281fdc0f23f53769b32f3c10033b4a39ab9fc3f

--- examples/verifly/incompatible_type.pl
+++ examples/verifly/incompatible_type.pl
@@ -1,11 +1,11 @@
 :- module(_,[p/1],[assertions,regtypes,functional,nativeprops]).
 
-:- pred p(X) => sorted(X) + (is_det).
+:- pred p(X) => sorted(X) + is_det.
 
 p(X) :-
     q(X).
 
-:- pred q(X) => color(X) + (is_det).
+:- pred q(X) => color(X) + is_det.
 
 q(M) :-
     M = red.
@@ -15,5 +15,5 @@ color := red | green | blue.
 
 :- regtype sorted/1.
 sorted := [] | [_].
-sorted([X,Y|T]) :-  X>Y, sorted([Y|T]).
+sorted([X,Y|T]) :-  X>=Y, sorted([Y|T]).
 
